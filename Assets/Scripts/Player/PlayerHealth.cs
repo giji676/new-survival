8ab7008a0e41a57da8b977f2a089c8043c4a725d
giji676 <tvtengoxachidze@gmail.com>
2023-04-23 22:59:29 +0100

    Hooked up the UI to the script

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,8 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour {
+    [SerializeField] private Image healthBar;
+    [SerializeField] private Image hungerBar;
+    [SerializeField] private Image hydrationBar;
+    [SerializeField] private TextMeshProUGUI healthText;
+    [SerializeField] private TextMeshProUGUI hungerText;
+    [SerializeField] private TextMeshProUGUI hydrationText;
     [SerializeField] private int maxHealth;
     [SerializeField] private int maxHunger;
     [SerializeField] private int maxHydration;
@@ -14,40 +22,56 @@ public class PlayerHealth : MonoBehaviour {
         currentHealth = maxHealth;
         currentHunger = maxHunger;
         currentHydration = maxHydration;
+
+        healthText.text = currentHealth.ToString();
+        hungerText.text = currentHunger.ToString();
+        hydrationText.text = currentHydration.ToString();
+        
+        healthBar.fillAmount = 100;
+        hungerBar.fillAmount = 100;
+        hydrationBar.fillAmount = 100;
+
+        DecreaseHealth(12);
     }
 
     public void IncreaseHealth(int point) {
-        currentHealth = Increase(0, maxHealth, currentHealth, point);
+        currentHealth = Increase(0, maxHealth, currentHealth, point, healthBar, healthText);
     }
 
     public void IncreaseHunger(int point) {
-        currentHunger = Increase(0, maxHunger, currentHunger, point);
+        currentHunger = Increase(0, maxHunger, currentHunger, point, hungerBar, hungerText);
     }
     
     public void IncreaseHydration(int point) {
-        currentHydration = Increase(0, maxHydration, currentHydration, point);
+        currentHydration = Increase(0, maxHydration, currentHydration, point, hydrationBar, hydrationText);
     }
     
     public void DecreaseHealth(int point) {
-        currentHealth = Decrease(0, maxHealth, currentHealth, point);
+        currentHealth = Decrease(0, maxHealth, currentHealth, point, healthBar, healthText);
     }
     
     public void DecreaseHunger(int point) {
-        currentHunger = Decrease(0, maxHunger, currentHunger, point);
+        currentHunger = Decrease(0, maxHunger, currentHunger, point, hungerBar, hungerText);
     }
     
     public void DecreaseHydration(int point) {
-        currentHydration = Decrease(0, maxHydration, currentHydration, point);
+        currentHydration = Decrease(0, maxHydration, currentHydration, point, hydrationBar, hydrationText);
     }
 
-    private int Increase(int min, int max, int current, int val) {
+    private int Increase(int min, int max, int current, int val, Image bar, TextMeshProUGUI text) {
         current += val;
-        return Mathf.Clamp(current, min, max);
+        current = Mathf.Clamp(current, min, max);
+        bar.fillAmount = Mathf.InverseLerp(min, max, current);
+        text.text = current.ToString();
+        return current;
     }
 
-    private int Decrease(int min, int max, int current, int val) {
+    private int Decrease(int min, int max, int current, int val, Image bar, TextMeshProUGUI text) {
         current -= val;
-        return Mathf.Clamp(current, min, max);
+        current = Mathf.Clamp(current, min, max);
+        bar.fillAmount = Mathf.InverseLerp(min, max, current);
+        text.text = current.ToString();
+        return current;
     }
 
 

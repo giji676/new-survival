2b847ba02171be36fdf1a1db491dc517e9afa8ea
giji676 <tvtengoxachidze@gmail.com>
2023-05-31 17:00:08 +0100

    Initial animal AI test

--- Assets/Scripts/Interactables/Dummy.cs
+++ Assets/Scripts/Interactables/Dummy.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Dummy : Interactable {
-    int health = 1000;
+    [SerializeField] private int health = 100;
 
     protected override void Interact(GameObject interactingObject, InteractionType interactionType) {
         base.Interact(interactingObject, interactionType);
         HandHeld handHeld = interactingObject.GetComponent<HandHeld>();
+        Debug.Log(handHeld);
         if (handHeld) TakeDamage(handHeld.damage);
     }
 
     public void TakeDamage(int damage) {
         health -= damage;
         print(health);
+        if (health <= 0) Destroy(gameObject);
     }
 }

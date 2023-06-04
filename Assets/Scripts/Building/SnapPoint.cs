e6ac1ff3ef253fd6d7dc9efcc67edb8c9d0ba998
giji676 <tvtengoxachidze@gmail.com>
2023-06-04 17:05:50 +0100

    Added door frame to building

--- Assets/Scripts/Building/SnapPoint.cs
+++ Assets/Scripts/Building/SnapPoint.cs
@@ -4,23 +4,4 @@ using UnityEngine;
 
 public class SnapPoint : MonoBehaviour {
     public Transform point;
-    public LayerMask buildingLayerMask;
-    public GameObject parent;
-    Vector3 size;
-
-    void Start() {
-        size = transform.lossyScale;
-    }
-    
-    private void Update()
-    {
-        return;
-        // Collider[] colliders = Physics.OverlapBox(transform.position, size / 2, transform.rotation, buildingLayerMask);
-
-        // foreach (Collider collider in colliders) {
-        //     if (collider.gameObject != parent) {
-        //         gameObject.GetComponent<Collider>().enabled = false;
-        //     }
-        // }
-    }
 }

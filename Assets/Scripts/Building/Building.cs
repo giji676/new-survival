f6bffb7124978d89fbb7e55e631f77003fd33bc1
giji676 <tvtengoxachidze@gmail.com>
2023-07-06 23:50:49 +0100

    Fixed preview collision, so now the ray continues if the building preview hits the wrong snapp collider

--- Assets/Scripts/Building/Building.cs
+++ Assets/Scripts/Building/Building.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Building : MonoBehaviour {
     public BuildingData buildingData;
     public float rayDistance = 5f;
+    float curDistance;
     private Camera cam;
     private Collider targetCollider;
 
@@ -18,25 +19,35 @@ public class Building : MonoBehaviour {
     }
 
     private void Build() {
+        curDistance = rayDistance;
         Ray ray = new Ray(cam.transform.position, cam.transform.forward);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, rayDistance, buildingData.colliderLayerMask)) {
-            targetCollider = hit.collider;
-            if (!targetCollider.GetComponent<SnapPoint>()) return;
-            SnapPoint snapPoint = targetCollider.GetComponent<SnapPoint>();
-            if (snapPoint.buildingTag != buildingData.targetTag) return;
-            targetCollider = hit.collider;
+        bool newRay = true; // If the ray hits a different snapping collider, continue the ray from the hit point with the remaining distance
+        while (newRay) {
+            if (Physics.Raycast(ray, out hit, curDistance, buildingData.colliderLayerMask)) {
+                targetCollider = hit.collider;
+                if (!targetCollider.GetComponent<SnapPoint>()) return; // If it doesn't have a snap point return
+                SnapPoint snapPoint = targetCollider.GetComponent<SnapPoint>();
+                if (snapPoint.buildingTag != buildingData.targetTag) { // If it is a different snapping collider
+                    curDistance = rayDistance - hit.distance; // Get the remaining distance
+                    ray = new Ray(hit.point + cam.transform.forward * 0.1f, cam.transform.forward); // Set the ray to the new ray
+                    
+                    continue;
+                }
+                targetCollider = hit.collider;
 
-            transform.position = snapPoint.point.position;
-            transform.rotation = Quaternion.Euler(0, snapPoint.point.eulerAngles.y, 0);
-        }
-        else if (Physics.Raycast(ray, out hit, rayDistance)) {
-            transform.rotation = Quaternion.Euler(0, cam.transform.eulerAngles.y, 0);
-            transform.position = hit.point;
-            targetCollider = null;
-        }
-        else {
-            transform.rotation = Quaternion.Euler(0, cam.transform.eulerAngles.y, 0);
+                transform.position = snapPoint.point.position;
+                transform.rotation = Quaternion.Euler(0, snapPoint.point.eulerAngles.y, 0);
+            }
+            else if (Physics.Raycast(ray, out hit, rayDistance)) {
+                transform.rotation = Quaternion.Euler(0, cam.transform.eulerAngles.y, 0);
+                transform.position = hit.point;
+                targetCollider = null;
+            }
+            else {
+                transform.rotation = Quaternion.Euler(0, cam.transform.eulerAngles.y, 0);
+            }
+            newRay = false;
         }
 
         if (Input.GetMouseButtonDown(0)) {
